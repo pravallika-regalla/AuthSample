6c76ca4160924c814d0b8a3595935ab6fd5a57ee
Ravikiran Pilli <rpilli+mmsrx@primerx.io>
2025-05-20 10:33:27 -0400

    changes

--- src/app/auth-routing.module.ts
+++ src/app/auth-routing.module.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AuthHttpInterceptor, AuthModule } from '@auth0/auth0-angular';
@@ -11,31 +10,18 @@ const config = {
   redirectUri: 'https://pravallika-regalla.github.io/AuthSample/home',
   httpInterceptor: {
     allowedList: ['/api/*']
-  },
+  }
 };
 
-const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  {
-    path: 'home',
-    component: HomeComponent
-  }
-];
-
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
+  declarations: [HomeComponent],
   imports: [
     CommonModule,
     HttpClientModule,
-    AuthModule.forRoot(config),
-    RouterModule.forRoot(routes, { useHash: true })
-
+    AuthModule.forRoot(config)
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthHttpInterceptor, multi: true }
-  ],
-  exports: [RouterModule]
+  ]
 })
-export class AuthRoutingModule { }
+export class AuthRoutingModule {}
