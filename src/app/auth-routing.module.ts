f451a03bf48549e37aa35a1c111bf1b0e2646c90
Ravikiran Pilli <rpilli+mmsrx@primerx.io>
2025-05-19 20:10:51 -0400

    changes

--- src/app/auth-routing.module.ts
+++ src/app/auth-routing.module.ts
@@ -8,7 +8,7 @@ import { AuthHttpInterceptor, AuthModule } from '@auth0/auth0-angular';
 const config = {
   domain: 'dev-gwrovjmwec7k1moz.us.auth0.com',
   clientId: 'jxiNuZ67b43Z4uL14r60ocai98BKdQp2',
-  redirectUri: window.location.origin+'/home',
+  redirectUri: 'https://pravallika-regalla.github.io/AuthSample/home',
   httpInterceptor: {
     allowedList: ['/api/*']
   },
